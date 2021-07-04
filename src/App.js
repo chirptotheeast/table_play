328e20a717d373c8920e43fa499e608e4f982077
chirptotheeast <alyssa.easterly@yahoo.com>
2021-07-04 09:44:16 -0400

    html updates

--- src/App.js
+++ src/App.js
@@ -3,6 +3,189 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+          <div class="wrapper">
+  <div class="table_title">Definitions</div>
+  <div class="table_wrap">
+    <div class="table_header">
+      <div class="item_wrap">
+            <div class="item item_term">Term</div>
+            <div class="vertical_line"></div>
+            <div class="item item_definition">Definition</div>
+      </div>
+    </div>
+ 
+  
+    <div class="table_body">
+         <div class="item_container">
+          <div class="item_left">
+            <div class="item item_term"><p>Programmatic</p></div>
+          </div>
+          <div class="item_right">
+            <div class="item item_definition "><p>Buying media based on targeting audiences and not specific sites. It is automated and performance can be adjusted in real time.</p> </div>
+          </div>
+         </div>
+    </div>
+  </div>
+  
+   <div class="table_wrap">
+    <div class="table_header">
+      <div class="item_wrap">
+        <div class="item item_term">Dimension</div>
+        <div class="vertical_line"></div>
+        <div class="item item_definition">Definition</div>
+      </div>
+    </div>
+ 
+  
+    <div class="table_body">
+        <div class="item_container">
+          <div class="item_left">
+            <div class="item item_term"><p>DV360</p></div>
+          </div>
+          <div class="item_right">
+            <div class="item item_definition"><p>Google's Programmatic Platform/DSP</p> </div>
+          </div>
+        </div>
+        <div class="item_container">
+          <div class="item_left">
+            <div class="item item_term"><p>TTD (The Trade Desk)</p></div>
+          </div>
+          <div class="item_right">
+            <div class="item item_definition"><p>DSP - Demand Side Platform</p> </div>
+            </div>
+        </div>
+        <div class="item_container">
+          <div class="item_left">
+            <div class="item item_term"><p>Teads</p></div>
+          </div>
+          <div class="item_right">
+            <div class="item item_definition"><p>Programmatic buying platform that specializes in Video</p> </div>
+          </div>
+        </div>
+        <div class="item_container">
+          <div class="item_left">
+          <div class="item item_term"><p>YouTube</p></div>
+          </div>
+          <div class="item_right">
+          <div class="item item_definition "><p>Video sharing service where users can watch, like, share, comment and upload their own videos.</p> </div>
+          </div>
+        </div>
+    </div>
+  </div>
+  
+    <div class="table_wrap">
+    <div class="table_header">
+      <div class="item_wrap">
+        <div class="item item_term">Metric</div>
+        <div class="vertical_line"></div>
+        <div class="item item_definition">Definition</div>
+      </div>
+    </div>
+ 
+  
+    <div class="table_body">
+       <div class="item_container">
+         <div class="item_left">
+          <div class="item item_term"><p>Impressions</p></div>
+         </div>
+         <div class="item_right">
+          <div class="item item_definition"><p>The number of times an ad was viewed when it enters a person's screen. This is different than Reach, because the same user can see an ad multiple times.</p> </div>
+          </div>
+       </div>
+       
+        <div class="item_container">
+          <div class="item_left">
+            <div class="item item_term"><p>Click</p></div>
+          </div>
+          <div class="item_right">
+            <div class="item item_definition"><p>	When a user clicks on an ad and is redirected to the destination URL of an ad.</p> </div>
+          </div>
+       </div>
+       
+        <div class="item_container">
+          <div class="item_left">
+            <div class="item item_term"><p>Click Through Rate (CTR)</p></div>
+          </div>
+          <div class="item_right">
+            <div class="item item_definition"><p>CTR is the percentage of total people visiting a web page who access a link to a particular advertisement, calculated as clicks divided by impressions (100 clicks from 1000 impressions equals a 10% CTR).</p> </div>
+          </div>
+       </div>
+       
+        <div class="item_container">
+          <div class="item_left">
+          <div class="item item_term"><p>Cost Per Click (CPC)</p></div>
+          </div>
+          <div class="item_right">
+          <div class="item item_definition"><p>Total cost divided by clicks ($100 for 50 clicks equals $2 CPC).</p> </div>
+          </div>
+       </div>
+       
+        <div class="item_container">
+          <div class="item_left">
+          <div class="item item_term"><p>Cost Per Completed View (CPCV)</p></div>
+          </div>
+          <div class="item_right">
+          <div class="item item_definition"><p>Cost paid every time a video ad runs through to completion.</p> </div>
+          </div>
+       </div>
+       
+        <div class="item_container">
+          <div class="item_left">
+            <div class="item item_term"><p>Cost Per Thousand Impressions (CPM)eted View (CPCV)</p></div>
+          </div>
+          <div class="item_right">
+            <div class="item item_definition"><p>The average cost per thousand impressions, calculated as the total cost/thousand impressions.</p> </div>
+          </div>
+       </div>
+       
+        <div class="item_container">
+          <div class="item_left">
+            <div class="item item_term"><p>Cost Per View (CPV)</p></div>
+          </div>
+          <div class="item_right">
+            <div class="item item_definition"><p>Pricing model in which advertisers are charged only when users watch the video. The Ad Exchange does not support CPV.</p> </div>
+          </div>
+       </div>
+       
+         <div class="item_container">
+          <div class="item_left">
+            <div class="item item_term"><p>Video Completion Rate (VCR)</p></div>
+          </div>
+          <div class="item_right">
+            <div class="item item_definition"><p>Percentage of times the video played to the end. Calculated as: video completes / video starts.</p> </div>
+          </div>
+       </div>
+       
+        <div class="item_container">
+          <div class="item_left">
+            <div class="item item_term"><p>View Through Rate (VTR)</p></div>
+          </div>
+          <div class="item_right">
+            <div class="item item_definition"><p>The number of completed views of an ad over the number of initial impressions. VTR = Completed views (user did not skip/pass by) / Impressions (ad rendered).</p> </div>
+          </div>
+       </div>
+       
+        <div class="item_container">
+          <div class="item_left">
+            <div class="item item_term"><p>Reach</p></div>
+          </div>
+          <div class="item_right">
+            <div class="item item_definition"><p>The number of unique users within an audience universe the ad was shown to.</p> </div>
+          </div>
+       </div>
+       
+        <div class="item_container">
+          <div class="item_left">
+            <div class="item item_term"><p>Frequency</p></div>
+          </div>
+          <div class="item_right">
+            <div class="item item_definition"><p>The average number of times individuals are exposed to the advertising message. </p> </div>
+          </div>
+       </div>
+    </div>
+  </div>
+  
+</div>
     </div>
   );
 }
